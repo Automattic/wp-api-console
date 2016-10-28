28c31b168e86f7125f47590db4472188c648b838
Riad Benguella <benguella@gmail.com>
2016-10-28 12:42:01 +0100

    JSON Viewer : Show a preview of the content when collapsed

---
+++ src/components/results/tests/utils.test.js
@@ -0,0 +1,34 @@
+import { stringify } from '../utils';
+
+it('should stringify an object recursively', () => {
+  const object = {
+    'a': 'b',
+    'c': {
+      'd': 'e'
+    }
+  };
+  const expected = '{<span class="key">"a"</span>:<span class="string">"b"</span>, <span class="key">"c"</span>:{<span class="key">"d"</span>:<span class="string">"e"</span>}}';
+  expect(stringify(object)).toEqual(expected);
+});
+
+
+it('should stringify an array recursively', () => {
+  const object = [
+    'a',
+    'b',
+    ['c', 'd']
+  ];
+  const expected = '[<span class="key">"0"</span>:<span class="string">"a"</span>, <span class="key">"1"</span>:<span class="string">"b"</span>, <span class="key">"2"</span>:[<span class="key">"0"</span>:<span class="string">"c"</span>, <span class="key">"1"</span>:<span class="string">"d"</span>]]';
+  expect(stringify(object)).toEqual(expected);
+});
+
+it('should crop the output if we exceed max', () => {
+  const object = {
+    'a': 'b',
+    'c': {
+      'd': 'e'
+    }
+  };
+  const expected = '{<span class="key">"a"</span>:<span class="string">"b"</span> …}';
+  expect(stringify(object, 5)).toEqual(expected);
+});
