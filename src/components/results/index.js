3f5eb6d56f14ede099b44d07fe5f373c56cebdf4
James Nylen <jnylen@gmail.com>
2016-11-15 15:40:14 -0600

    Allow selecting collapsed JSON text (part 2)
    This cancels click events on collapsed JSON trees if the document has
    some selected text.
    
    Since the click handler is inside `react-json-tree` rather than code we
    control, this is done using a document-level event handler with
    `useCapture` set to true so that we can intercept the click event before
    it fires.
    
    See also:
    
    - http://stackoverflow.com/a/4616720
    - https://github.com/Automattic/rest-api-console2/pull/23

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -9,70 +9,88 @@ import RequestHeader from './header';
 import { getResults } from '../../state/results/selectors';
 import { escapeLikeJSON, stringify } from './utils';
 
-const Results = ( { results } ) => {
-	const jsonTheme = {
-		extend: 'default',
-		tree: {
-			MozUserSelect: 'text',
-			WebkitUserSelect: 'text',
-		},
-		nestedNodeItemString: ( node, expandedNodes, type, expanded ) => {
-			return {
-				className: expanded ? 'expanded' : 'collapsed',
-			};
-		},
-	};
+class Results extends React.Component {
+	componentWillMount() {
+		document.addEventListener( 'click', this.overrideClickIfSelected, true );
+	}
 
-	const expandFalse = () => false;
-	const getItemString = ( type, data, itemType, itemString ) => (
-		/* eslint-disable react/no-danger */
-		<span
-			className="collapsed-content"
-			dangerouslySetInnerHTML={ { __html: `${ itemString } <span class="content">${ stringify( data ) }</span>` } }
-		/>
-		/* eslint-enable react/no-danger */
-	);
+	componentWillUnmount() {
+		document.removeEventListener( 'click', this.overrideClickIfSelected );
+	}
 
-	const valueRenderer = value => {
-		if ( typeof value === 'string' ) {
-			const valueWithoutQuotes = value.replace( /^"|"$/g, '' );
-			let valueEscaped;
-			if ( valueWithoutQuotes === value ) {
-				// Probably a boolean or number
-				valueEscaped = escapeLikeJSON( value );
-			} else {
-				// String
-				valueEscaped = '"' + escapeLikeJSON( valueWithoutQuotes ) + '"';
-			}
-			return (
-				<span className="expanded-value">{ valueEscaped }</span>
-			);
+	overrideClickIfSelected = event => {
+		if ( window.getSelection().toString().length ) {
+			event.stopPropagation();
 		}
-
-		return value;
 	};
 
-	return (
-		<div className="results">
-			{ results.map( ( result, index ) =>
-				<div key={ result.id } className={ classnames( 'request', { error: result.response && !! result.response.error } ) }>
-					<RequestHeader result={ result } />
-					{ result.response && result.response.body &&
-						<div className="response">
-							<JSONTree
-								theme={ jsonTheme }
-								data={ result.response.body }
-								shouldExpandNode={ expandFalse }
-								getItemString={ getItemString }
-								valueRenderer={ valueRenderer }
-							/>
-						</div>
-					}
-				</div>
-			)}
-		</div>
-	);
-};
+	render() {
+		const { results } = this.props;
+
+		const jsonTheme = {
+			extend: 'default',
+			tree: {
+				MozUserSelect: 'text',
+				WebkitUserSelect: 'text',
+			},
+			nestedNodeItemString: ( node, expandedNodes, type, expanded ) => {
+				return {
+					className: expanded ? 'expanded' : 'collapsed',
+				};
+			},
+		};
+
+		const expandFalse = () => false;
+		const getItemString = ( type, data, itemType, itemString ) => (
+			/* eslint-disable react/no-danger */
+			<span
+				className="collapsed-content"
+				dangerouslySetInnerHTML={ { __html: `${ itemString } <span class="content">${ stringify( data ) }</span>` } }
+			/>
+			/* eslint-enable react/no-danger */
+		);
+
+		const valueRenderer = value => {
+			if ( typeof value === 'string' ) {
+				const valueWithoutQuotes = value.replace( /^"|"$/g, '' );
+				let valueEscaped;
+				if ( valueWithoutQuotes === value ) {
+					// Probably a boolean or number
+					valueEscaped = escapeLikeJSON( value );
+				} else {
+					// String
+					valueEscaped = '"' + escapeLikeJSON( valueWithoutQuotes ) + '"';
+				}
+				return (
+					<span className="expanded-value">{ valueEscaped }</span>
+				);
+			}
+
+			return value;
+		};
+
+		return (
+			<div className="results">
+				{ results.map( ( result, index ) =>
+					<div key={ result.id } className={ classnames( 'request', { error: result.response && !! result.response.error } ) }>
+						<RequestHeader result={ result } />
+						{ result.response && result.response.body &&
+							<div className="response">
+								<JSONTree
+									theme={ jsonTheme }
+									data={ result.response.body }
+									shouldExpandNode={ expandFalse }
+									getItemString={ getItemString }
+									valueRenderer={ valueRenderer }
+								/>
+							</div>
+						}
+					</div>
+				)}
+			</div>
+		);
+	}
+}
 
 export default connect(
 	state => {
