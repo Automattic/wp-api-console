6355807af54b7ea4ef645e62f57887a6c5ae52d5
James Nylen <jnylen@gmail.com>
2017-02-14 18:42:48 -0600

    Fix version 1.2 and higher endpoints (#66)
    * Bring authProvider into scope of .com API object
    
    * Make an authenticated request to /versions
    
    * Fix the tests

--- src/api/com.js
+++ src/api/com.js
@@ -1,54 +1,49 @@
-import superagent from 'superagent';
-
 const baseUrl = 'https://public-api.wordpress.com/rest/';
 
-export const api = {
-	name: 'WP.COM API',
-	getDiscoveryUrl: version => baseUrl + version + '/help',
-	parseEndpoints: data => {
-		const documented = [];
-		const undocumented = [];
-		const noGroup = [];
-		data.forEach( endpoint => {
-			endpoint.pathFormat = endpoint.path_format;
-			delete endpoint.path_format;
-			endpoint.pathLabeled = endpoint.path_labeled;
-			delete endpoint.path_labeled;
-			if ( endpoint.group === '__do_not_document' ) {
-				undocumented.push( endpoint );
-			} else if ( endpoint.group ) {
-				documented.push( endpoint );
-			} else {
-				noGroup.push( endpoint );
-			}
-		} );
-		return documented.concat( noGroup ).concat( undocumented );
-	},
-	loadVersions: () =>
-		superagent.get( baseUrl + 'v1.1/versions?include_dev=true' )
-			.set( 'accept', 'application/json' )
-			.then( res => {
+const createApi = authProvider => {
+	return {
+		authProvider,
+		name: 'WP.COM API',
+		getDiscoveryUrl: version => baseUrl + version + '/help',
+		parseEndpoints: data => {
+			const documented = [];
+			const undocumented = [];
+			const noGroup = [];
+			data.forEach( endpoint => {
+				endpoint.pathFormat = endpoint.path_format;
+				delete endpoint.path_format;
+				endpoint.pathLabeled = endpoint.path_labeled;
+				delete endpoint.path_labeled;
+				if ( endpoint.group === '__do_not_document' ) {
+					undocumented.push( endpoint );
+				} else if ( endpoint.group ) {
+					documented.push( endpoint );
+				} else {
+					noGroup.push( endpoint );
+				}
+			} );
+			return documented.concat( noGroup ).concat( undocumented );
+		},
+		loadVersions: () =>
+			authProvider.request( {
+				method: 'GET',
+				url: baseUrl + 'v1.1/versions?include_dev=true',
+			} ).then( res => {
 				return {
 					versions: res.body.versions.map( version => `v${ version }` ),
 					current: `v${ res.body.current_version }`,
 				};
 			} ),
-	buildRequest: ( version, method, path, body ) => {
-		return {
-			url: baseUrl + version + path,
-			apiVersion: version.substr( 1 ),
-			method,
-			path,
-			body,
-		};
-	},
-	baseUrl,
-};
-
-const createApi = authProvider => {
-	return {
-		authProvider,
-		...api,
+		buildRequest: ( version, method, path, body ) => {
+			return {
+				url: baseUrl + version + path,
+				apiVersion: version.substr( 1 ),
+				method,
+				path,
+				body,
+			};
+		},
+		baseUrl,
 	};
 };
 
