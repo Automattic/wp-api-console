bd39722620ada5bf823375e257c0ee495ab7ff5a
Daniel López <daniel.lopez@automattic.com>
2022-06-23 13:58:23 +0200

    Update to React 18.2

--- src/components/param-builder/input.jsx
+++ src/components/param-builder/input.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import TagsInput from 'react-tagsinput';
 import 'react-tagsinput/react-tagsinput.css';
 
