840d4366e83eaef4d5e2553efcdc62d080f15c45
Matthew Reishus <mreishus@users.noreply.github.com>
2023-11-13 10:56:54 -0600

    Support sharing madlibs paths

--- src/components/share/index.jsx
+++ src/components/share/index.jsx
@@ -3,12 +3,29 @@ import { connect } from 'react-redux';
 
 import './style.css';
 
-const Share = ( { api, version, method, url } ) => {
+const Share = ( { api, version, method, url, pathLabeled, pathValues, queryParams } ) => {
 	const [ buttonText, setButtonText ] = useState( 'Share' );
 
+	// The API console has two methods, 1. Directly type in the URL, 2. Use the form to build the URL.
+	// I'm calling 2 the "madlibs" method, because it's like filling in the blanks of a story.
+	// This function constructs the madlibs path if we can't use the direct request.url.
+	const constructMadlibsPath = () => {
+		let path = pathLabeled;
+		Object.keys( pathValues ).forEach( ( key ) => {
+			path = path.replace( key, encodeURIComponent( pathValues[ key ] ) );
+			console.log( key, pathValues[ key ], path );
+		} );
+		if ( queryParams ) {
+			const queryParamsString = new URLSearchParams( queryParams ).toString();
+			path += `?${ queryParamsString }`;
+		}
+		return path;
+	};
+
 	const buildShareableLink = () => {
-		const baseUrl = window.location.href.split( '?' )[ 0 ];
-		const queryParams = new URLSearchParams( { api, version, method, url } ).toString();
+		const finalPath = url || constructMadlibsPath();
+		const baseUrl = window.location.origin;
+		const queryParams = new URLSearchParams( { api, version, method, url: finalPath } ).toString();
 		const shareableUrl = `${ baseUrl }?${ queryParams }`;
 
 		navigator.clipboard
@@ -32,6 +49,9 @@ const mapStateToProps = ( state ) => ( {
 	version: state.ui.version,
 	method: state.request.method,
 	url: state.request.url,
+	pathValues: state.request?.pathValues,
+	queryParams: state.request?.queryParams,
+	pathLabeled: state.request?.endpoint?.pathLabeled,
 } );
 
 export default connect( mapStateToProps )( Share );
