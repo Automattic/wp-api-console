39b68c9b749642c28215f86c11292665f2f80f93
Gabriel Caires <gabriel.caires@automattic.com>
2026-09-01 10:44:00 +0100

    Upgrade runtime and add dialog dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app';
 
-ReactDOM.render(
-	<App />,
-	document.getElementById( 'root' )
-);
+createRoot( document.querySelector( '#root' ) ).render( <App /> );
